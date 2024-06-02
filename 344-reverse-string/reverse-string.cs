230b64b33b2420da33871b749fb7b7ea200d2001
shashith-chandra <kscpinku25@gmail.com>
2024-06-02 21:45:34 +0530

    Time: 222 ms (66.24%) | Memory: 129.1 MB (88.05%) - LeetSync

---
+++ 344-reverse-string/reverse-string.cs
@@ -0,0 +1,13 @@
+public class Solution {
+    public void ReverseString(char[] s) {
+        int i = 0, j = s.Length-1;
+        while(i<j)
+        {
+            char temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
+            i++;
+            j--;
+        }
+    }
+}
