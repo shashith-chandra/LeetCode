883a768e75da285f0d2a8bfe89b0b55767776241
shashith-chandra <kscpinku25@gmail.com>
2024-06-23 19:00:05 +0530

    Time: 15 ms (70.71%) | Memory: 24.4 MB (34.46%) - LeetSync

---
+++ 36-valid-sudoku/valid-sudoku.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+      
+        vector<set<int>> rows(9), cols(9), boxes(9);
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] == '.') continue;
+
+                int num = board[i][j] - '0';
+                int boxIndex = (i / 3) * 3 + j / 3;
+
+                if (rows[i].count(num) || cols[j].count(num) || boxes[boxIndex].count(num)) {
+                    return false;
+                }
+
+                rows[i].insert(num);
+                cols[j].insert(num);
+                boxes[boxIndex].insert(num);
+            }
+        }
+
+        return true;
+    }
+};
+   
