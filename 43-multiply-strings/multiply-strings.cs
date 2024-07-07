a70a64477f886e3db2cd6de830e266e28f4dca8a
shashith-chandra <kscpinku25@gmail.com>
2024-07-07 23:44:04 +0530

    Time: 57 ms (80.26%) | Memory: 41.2 MB (81.03%) - LeetSync

---
+++ 43-multiply-strings/multiply-strings.cs
@@ -0,0 +1,7 @@
+public class Solution {
+    public string Multiply(string num1, string num2) {
+       
+         return (BigInteger.Parse(num1) * BigInteger.Parse(num2)).ToString();
+        
+    }
+}
