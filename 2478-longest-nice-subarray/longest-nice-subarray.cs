205456efb1d86c2365a1fd75fbdb2d2503bb533d
shashith-chandra <kscpinku25@gmail.com>
2025-03-19 00:49:11 +0530

    Time: 1 ms (100.00%) | Memory: 63.1 MB (50.00%) - LeetSync

---
+++ 2478-longest-nice-subarray/longest-nice-subarray.cs
@@ -0,0 +1,25 @@
+public class Solution {
+    public int LongestNiceSubarray(int[] nums) {
+        int usedBits = 0; // Tracks bits used in current window
+        int windowStart = 0; // Start position of current window
+        int maxLength = 0; // Length of longest nice subarray found
+
+        for (int windowEnd = 0; windowEnd < nums.Length; ++windowEnd) {
+            // If current number shares bits with window, shrink window from left
+            // until there's no bit conflict
+            while ((usedBits & nums[windowEnd]) != 0) {
+                usedBits ^= nums[windowStart]; // Remove leftmost element's bits
+                windowStart++;
+            }
+
+            // Add current number to the window
+            usedBits |= nums[windowEnd];
+
+            // Update max length if current window is longer
+            maxLength = Math.Max(maxLength, windowEnd - windowStart + 1);
+        }
+
+        return maxLength;
+        
+    }
+}
